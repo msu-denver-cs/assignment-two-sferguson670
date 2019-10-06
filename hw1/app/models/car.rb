95e5b17bc5eb08ccca2e4cc5d552490c67e2d427
sferguson670 <sferguson670@gmail.com>
2019-10-05 21:05:09 -0600

    fixed validations?
    change car validations

--- hw1/app/models/car.rb
+++ hw1/app/models/car.rb
@@ -1,14 +1,11 @@
 class Car < ActiveRecord::Base
   belongs_to :make
-  
-  validates: name, presence: true
-  validates: model, presence: true
-  validates: vin, numericality: { only_integer: true }
-  validates: make, presence: true
 end
 
 class Car < ActiveRecord::Base
     has_many:factors
     has_many:parts, through: :factors
+    
+    validates_associated :parts
 end
 
