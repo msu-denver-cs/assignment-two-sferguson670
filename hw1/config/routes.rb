a5ee292d6f88bb8ca4b818e97d38162b19462077
sferguson670 <sferguson670@gmail.com>
2019-10-06 21:12:02 -0600

    added search for makes
    search bar in makes index page

--- hw1/config/routes.rb
+++ hw1/config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  resources :cars
-  resources :makes
+  resources :cars 
+  resources :makes do
+      collection do
+          get 'search'
+      end
+  end
+  # search_makes GET /makes/search(.:format) makess#search
   resources :parts do
       collection do
           get 'search'
       end
-  end 
+  end
 # search_parts GET /parts/search(.:format) parts#search
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
