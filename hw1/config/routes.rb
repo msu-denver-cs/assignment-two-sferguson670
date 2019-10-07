86029f93797d77fc817b9cc0b0ae40ffabd29843
sferguson670 <sferguson670@gmail.com>
2019-10-06 20:54:27 -0600

    added search for parts
    on parts index

--- hw1/config/routes.rb
+++ hw1/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :cars
   resources :makes
-  resources :parts
+  resources :parts do
+      collection do
+          get 'search'
+      end
+  end 
+# search_parts GET /parts/search(.:format) parts#search
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
