aa4d2dff1f8c32d80e63c69cb492fb322fb15707
sferguson670 <sferguson670@gmail.com>
2019-10-06 22:07:19 -0600

    test for part validations
    done in part model test

--- hw1/test/models/part_test.rb
+++ hw1/test/models/part_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    
+    test "for empty name" do
+        p = Part.create({:name => ""})
+        refute p.valid?
+        refute p.save
+        assert_equal({:name=>["can't be blank", "only allows letters"]}, p.errors.messages)
+    end
+
+    test "for non-alphabetical name" do
+        p = Part.create({:name => "343"})
+        refute p.valid?
+        refute p.save
+        assert_equal({:name=>["only allows letters"]}, p.errors.messages)
+    end
 end
